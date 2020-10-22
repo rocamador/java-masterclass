5f59faf7d01db7031bef69a40c38dafec0bf08ca
rocamador <xrocamador@gmail.com>
2020-10-22 14:29:59 +0100

    updated

---
+++ 06/src/Dog.java
@@ -0,0 +1,17 @@
+public class Dog extends Animal {
+
+    private int eyes;
+    private int legs;
+    private int tail;
+    private int teeth;
+    private String coat;
+
+    public Dog(String name, int size, int weight, int eyes, int legs, int tail, int teeth, String coat) {
+        super(name, 1, 1, size, weight);
+        this.eyes = eyes;
+        this.teeth = teeth;
+        this.legs = legs;
+        this.coat = coat;
+        this.tail = tail;
+    }
+}
